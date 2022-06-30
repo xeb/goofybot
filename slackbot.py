b7f37f4ef41ced2acd13b46b1247baada15f84eb
xeb <xeb@hey.com>
2022-06-30 05:12:12 +0000

    Feature requests for dynamic engine and temp changes

--- slackbot.py
+++ slackbot.py
@@ -18,13 +18,32 @@ app = App(token=SLACK_BOT_TOKEN)
 def generate(prompt):
     config = toml.load("config.toml")["config"]
 
+    additional_temp = prompt.count(":fire:") * 0.05
+    temperature=config["temperature"] + additional_temp
+    prompt = prompt.replace(":fire:", "")
+
+    if temperature > 1:
+        temperature = 0.99
+
+    engine = config["default_engine"]
+
+    # If there's a config of emoji_engine then use that engine
+    for key in config.keys():
+        if "_engine" in key:
+            emoji = key[:-len("_engine")]
+            if f":{emoji}:" in prompt:
+                engine = config[key]
+                prompt = prompt.replace(f":{emoji}:", "").strip()
+
+    print(f"Using {prompt=}")
     response = openai.Completion.create(
-        engine=config["engine"],
+        engine=engine,
         prompt=prompt,
-        temperature=config["temperature"],
+        temperature=temperature,
         max_tokens=config["max_tokens"]
     )
 
+    print(f"Received {len(response.choices)=}")
     txt = response.choices[0].text
     return txt
 
@@ -38,10 +57,26 @@ def mention_handler_app_mention(body, say, logger):
         event["text"].replace(event["text"].split(" ")[0].strip(), "").strip()
     )  # i feel dirty but its late
     
+    if prompt.lower() == "help":
+        emoji_engines = ""
+        config = toml.load("config.toml")["config"]
+        for key in config.keys():
+            if "_engine" in key and key != "default_engine":
+                emoji = key[:-len("_engine")]
+                emoji_engines = emoji_engines + f":{emoji}: == '{config[key]}', \n"
+
+        emoji_engines = emoji_engines + f"\n and the default is {config['default_engine']}"
+
+        say(f"To use me, just mention me with @Goofybot and type your question. You can put :fire: to increase my craziness (up to 4 times). To change the engine, you can use the following emoji's:\n\n {emoji_engines}", thread_ts=thread_ts)
+        return
+
     app.client.reactions_add(channel=channel, timestamp=thread_ts, name="thumbsup")
     print(f"Generating {prompt=}")
-    response = generate(prompt)
-    say(response, thread_ts=thread_ts)
+    try:
+        response = generate(prompt)
+        say(response, thread_ts=thread_ts)
+    except Exception as e:
+        say(f"Uhoh. Error {e=}", thread_ts=thread_ts)
 
 
 @app.event("message")
@@ -56,7 +91,7 @@ def mention_handler_message(body, say):
     message = event["text"].strip()
     user = event.get("user", None)
 
-    print("{user=}\n{message=}\n{channel=}\n{thread_ts=}")
+    print(f"{user=}\n{message=}\n{channel=}\n{thread_ts=}")
     return # note I've disabled this until I can get the username parsing right & avoid a "double message"
     # I need to get the current user
 
